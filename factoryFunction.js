a58d7b4ecc9a9ce11b9faef217f47dd44990d6c0
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-02 12:35:56 +0500

    Built Factory functions

---
+++ factoryFunction.js
@@ -0,0 +1,29 @@
+function createCircle(radius, location) {
+  return {
+    radius,
+    location,
+    draw() { },
+    move() { },
+  };
+    
+}
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+	draw: function() { },
+	move: function() { },
+};
+
+const circle2 = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+	draw: function() { },
+	move: function() { },
+};

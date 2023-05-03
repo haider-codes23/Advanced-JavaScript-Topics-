4f78b669fb25d5bdefa82c677efd366a5ceac0c4
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-03 15:03:53 +0500

    Four Pillars of Object-Oriented-Programming

--- objectOrientedProgramming.js
+++ objectOrientedProgramming.js
@@ -9,7 +9,10 @@ function getWage(baseSalary, overTime, rate) {
 console.log(getWage(baseSalary, overTime, rate));
 
 // If we encapsualte all these variables and the functions into a single object then that would be called 
-// Encapsulation. 
+// Encapsulation. This is better as you can see that the getWage() method in the person object doesn't have
+// any poarameters, in contrast our getWage() function has 3 parameters. The reason why we didn't need
+// parameters was becasue all these parameters were actually modeled as properties of this person object
+// All these properties and getWage() method are highly related so they are part of one unit. 
 
 let person = {
   baseSalary: 30000,
@@ -21,3 +24,4 @@ let person = {
 }
 
 console.log(person.getWage());
+
